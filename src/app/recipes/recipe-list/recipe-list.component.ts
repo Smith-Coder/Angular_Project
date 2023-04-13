5d3fb29161ed1ba5ba9e964634f049a074f212c4
Smith C D <Smith.CD@trivium-esolutions.com>
2023-04-13 16:40:45 +0530

    second commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [new Recipe('A test recipe','This is simply a test','https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80'),
+  new Recipe('A test recipe','This is simply a test','https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80'),
+  new Recipe('A test recipe','This is simply a test','https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80')];
 }
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+export class Ingredient{
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name:string, amount:number){
+    //     this.name=name;
+    //     this.amount=amount;
+    // }
+
+    //instead of the above code we can do this in typeScript
+
+    constructor(public name:string, public amount:number){
+    }
+
+    
+}
